9d0f725e963221fd502c6b86cabdd57f0cc7369c
jarbalausa <balausanisa@gmail.com>
2024-09-26 18:31:03 +0600

    init

--- src/components/InfoBlock.jsx
+++ src/components/InfoBlock.jsx
@@ -11,24 +11,28 @@ const InfoBlock = () => {
     return (
         <div className='bg-gray-800'>
             {/* Library Section */}
-            <div className="flex justify-center items-center py-10 pt-20">
-                <div className="text-center">
-                    <div className="text-yellow-400 text-2xl sm:w-[500px] w-[300px]">Welcome to Our Library!</div>
-                    <div className="text-white text-sm sm:w-[400px] w-[300px] py-5">Discover a world of knowledge and adventure!</div>
-                    <Link to={'/newarrivals'} className="bg-green-500 rounded-full px-10 py-2 mr-3">New Arrivals</Link>
-                    <Link to={'/recommendations'} className="bg-white rounded-full px-10 py-2">Book Recommendations</Link>
+            <div className="flex flex-col md:flex-row justify-center items-center py-10 pt-20">
+                <div className="text-center md:text-left md:w-[500px] w-[300px]">
+                    <h1 className="text-yellow-400 text-2xl">Welcome to Our Library!</h1>
+                    <p className="text-white text-sm py-5">Discover a world of knowledge and adventure!</p>
+                    <div className="flex justify-center md:justify-start">
+                        <Link to={'/newarrivals'} className="bg-green-500 rounded-full px-10 py-2 mr-3">New Arrivals</Link>
+                        <Link to={'/recommendations'} className="bg-white rounded-full px-10 py-2">Book Recommendations</Link>
+                    </div>
                 </div>
-                <div>
+                <div className="md:ml-10">
                     <img className='md:block hidden rounded-3xl' src="/images/book1.jpg" alt="Library" width={400} />
                 </div>
             </div>
+
             <div className='flex justify-center'>
-                <div className='w-[925px] pb-10 text-white'>
-                    Reading is not just a pastime; it’s a way to expand your horizons and grow intellectually. Dive into our various genres and find your next favorite book!
+                <div className='w-[925px] pb-10 text-white text-center'>
+                    <p>Reading is not just a pastime; it’s a way to expand your horizons and grow intellectually. Dive into our various genres and find your next favorite book!</p>
                 </div>
             </div>
-            <div className='text-3xl text-white ml-[200px]'>Featured Books</div>
-            <div className='flex justify-center'>
+
+            <h2 className='text-3xl text-white text-center mb-5'>Featured Books</h2>
+            <div className='flex flex-wrap justify-center'>
                 {books.map((book, index) => (
                     <div key={index} className="border rounded-lg shadow-lg overflow-hidden bg-white m-5">
                         <img src={book.image} alt={book.title} className='w-[200px] h-[300px] object-cover' />
