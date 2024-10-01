ec56cbb9128a65a06707361405184cb2ffc2a8fd
jarbalausa <balausanisa@gmail.com>
2024-10-01 10:38:26 +0600

    init

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,43 @@
-
-
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-6">
-        <div className="container mx-auto text-center">
-            <div className="mb-4"
-            ><img
-            src="/path/to/logo.png"
+      <div className="container mx-auto text-center">
+        <div className="mb-4">
+          <img
+            src="/images/book_logo.png"
             alt="Website Logo "
             className="w-24 mx-auto"
-            />
-            </div>
-            
-        
+          />
+        </div>
+
         <div className="mb-4">
-            <p>Contact Us:</p>
-            <p>Email :<a href="mailto:your-email@gmail.com" className="text-green-400">
-                balausanisa@gmail.com</a></p>
+          <p>Contact Us:</p>
+          <p>
+            Email :
+            <a href="mailto:your-email@gmail.com" className="text-green-400">
+              balausanisa@gmail.com
+            </a>
+          </p>
         </div>
         <div className="mb-4">
-            <p>Follow Us On:</p>
-            <a href="https://www.instagram.com/library.almaty/" className="text-green-400 mx-2">Instagram</a>
-            <a href="https://github.com/jarbalausa-github" className="text-green-400 mx-2">GitHub</a>
+          <p>Follow Us On:</p>
+          <a
+            href="https://www.instagram.com/library.almaty/"
+            className="text-green-400 mx-2"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://github.com/jarbalausa-github"
+            className="text-green-400 mx-2"
+          >
+            GitHub
+          </a>
         </div>
-        <div >
-            <p> Library IA . All rights reserved</p>
+        <div>
+          <p> Library IA . All rights reserved</p>
         </div>
-        </div>
+      </div>
     </footer>
   );
 };
